80ba28a17243bc3b5e54ea45f68bfcacbaccf9da
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-05-28 19:53:52 +0300

    Added base theme

---
+++ lib/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const defaultBackground = Color(0xFFF4F4F4);
---
+++ lib/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:subscription_monitoring/colors.dart';
+
+ThemeData theme() {
+  return ThemeData(scaffoldBackgroundColor: defaultBackground);
+}
