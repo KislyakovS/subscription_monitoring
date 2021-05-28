13bf4ef66653d216261eba3196c16eb6e7424ce4
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-05-28 22:09:07 +0300

    Update primaryColor

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const defaultBackground = Color(0xFFF4F4F4);
-const primaryColor = Color(0xFFb36bfe);
+const primaryColor = Color(0xFF6b61e2);
 
 const colorSelectedNavItem = Color(0xFF322c59);
 const colorUnselectedNavItem = Color(0xFFc5cfd9);
