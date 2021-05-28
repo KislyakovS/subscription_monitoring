3e8442950cdd95e3b86e268617a0caec61852f17
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-05-28 21:54:06 +0300

    Fix

--- lib/theme.dart
+++ lib/theme.dart
@@ -4,9 +4,5 @@ import 'package:subscription_monitoring/colors.dart';
 ThemeData theme() {
   return ThemeData(
     scaffoldBackgroundColor: defaultBackground,
-    bottomNavigationBarTheme: BottomNavigationBarThemeData(
-      backgroundColor: defaultBackground,
-      elevation: 0,
-    ),
   );
 }
