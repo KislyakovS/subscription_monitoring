963a4869c9a9f397cc43f37774e7a310967446f6
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-05-29 19:35:47 +0300

    Update subscription card

--- lib/components/subscription_card.dart
+++ lib/components/subscription_card.dart
@@ -1,78 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:subscription_monitoring/colors.dart';
+import 'package:subscription_monitoring/components/bouncing_button.dart';
 import 'package:subscription_monitoring/components/line_percent.dart';
+import 'package:subscription_monitoring/components/subscription_header.dart';
 import 'package:subscription_monitoring/models/Subscription.dart';
 
 class SubscriptionCard extends StatelessWidget {
+  final Subscription subscription;
+  final GestureTapCallback press;
+
+  const SubscriptionCard({
+    Key? key,
+    required this.subscription,
+    required this.press,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            blurRadius: 8,
-            offset: const Offset(0, 2),
-          )
-        ],
+    return BouncingButton(
+      press: press,
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 8,
+              offset: const Offset(0, 2),
+            )
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SubscriptionHeader(
+              imageSrc: subscription.imageSrc,
+              title: subscription.title,
+            ),
+            const SizedBox(height: 15),
+            _buildProgress(),
+            const SizedBox(height: 15),
+            _buildPrice()
+          ],
+        ),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+    );
+  }
+
+  Text _buildPrice() {
+    return Text.rich(
+      TextSpan(
+        text: '\$ ${subscription.price}',
+        style: const TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
         children: [
-          Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Image.network(
-                  demoSubscription.imageSrc,
-                  width: 70,
-                  height: 70,
-                  fit: BoxFit.fill,
-                ),
-              ),
-              const SizedBox(width: 10),
-              Text(
-                'Netflix',
-                style: TextStyle(
-                  fontSize: 23,
-                  fontWeight: FontWeight.w500,
-                ),
-              )
-            ],
-          ),
-          const SizedBox(height: 15),
-          Text('Оплата через 4 дня'),
-          const SizedBox(height: 5),
-          SizedBox(
-            width: double.infinity,
-            child: LinePercent(
-              fillColor: primaryColor.withOpacity(0.7),
-              lineColor: primaryColor,
-              procent: 0.8,
-            ),
-          ),
-          const SizedBox(height: 15),
-          Text.rich(
-            TextSpan(
-              text: '\$ 10.99',
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
-              children: [
-                TextSpan(
-                  text: '/mo',
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w400,
-                  ),
-                )
-              ],
+          TextSpan(
+            text: '/month',
+            style: const TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w400,
             ),
           )
         ],
       ),
+    );
+  }
+
+  Column _buildProgress() {
+    final remainedDat = subscription.endDate.difference(DateTime.now()).inDays;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Payment in $remainedDat days'),
+        const SizedBox(height: 5),
+        SizedBox(
+          width: double.infinity,
+          child: LinePercent(
+            fillColor: primaryColor.withOpacity(0.7),
+            lineColor: primaryColor,
+            procent: 0.8,
+          ),
+        )
+      ],
     );
   }
 }
