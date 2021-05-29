b39c2893fc3e2418d315b9545b10739479a08785
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-05-29 21:32:33 +0300

    Color status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:subscription_monitoring/colors.dart';
 import 'package:subscription_monitoring/theme.dart';
 
 import 'routes.dart';
 import 'screens/bottom_navigation/bottom_navigation_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    //Lets make the Status Bar Transparent
+    statusBarColor: Colors.transparent,
+    systemNavigationBarColor: defaultBackground,
+
+    //Lets make the status bar icon brightness to bright
+    statusBarIconBrightness: Brightness.light,
+    systemNavigationBarIconBrightness: Brightness.dark,
+  ));
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
