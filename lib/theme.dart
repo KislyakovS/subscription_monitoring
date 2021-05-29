44aa4821ba6e99096098b45059743f1b31cd5512
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-05-29 20:58:01 +0300

    Added theme appBar

--- lib/theme.dart
+++ lib/theme.dart
@@ -4,5 +4,14 @@ import 'package:subscription_monitoring/colors.dart';
 ThemeData theme() {
   return ThemeData(
     scaffoldBackgroundColor: defaultBackground,
+    appBarTheme: appBarTheme(),
+  );
+}
+
+AppBarTheme appBarTheme() {
+  return const AppBarTheme(
+    elevation: 0,
+    backgroundColor: defaultBackground,
+    brightness: Brightness.light,
   );
 }
