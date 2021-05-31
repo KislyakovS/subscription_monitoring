750b7e5a9819f41f65cae0a2a6a9d09fa14d156b
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-05-31 20:59:55 +0300

    Added base SubscriptionDetailsScreen

--- lib/screens/subscription_details_screen/subscription_details_screen.dart
+++ lib/screens/subscription_details_screen/subscription_details_screen.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:subscription_monitoring/models/Subscription.dart';
 
+import 'components/body.dart';
+
 class SubscriptionDetailsScreen extends StatelessWidget {
   static String routeName = '/subscription_details';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final arguments = ModalRoute.of(context)!.settings.arguments
+        as SubscriptionDetailsArguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          arguments.subscription.title,
+          style: const TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Body(
+        subscription: arguments.subscription,
+      ),
+    );
   }
 }
 
