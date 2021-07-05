949f9ebf2f90cff170d453c80fc8c2cc7ee4c7ec
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-07-05 19:55:24 +0300

    Fix ios system file

--- lib/screens/settings/components/body.dart
+++ lib/screens/settings/components/body.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:subscription_monitoring/service/storage.dart';
 import 'package:subscription_monitoring/theme/constants.dart';
 
 import './settings_list.dart';
 
 class Body extends StatelessWidget {
   Body({Key? key}) : super(key: key);
+
+  Future<void> _pressButton() async {
+    final storage = Storage();
+
+    //final isNotification = await storage.getSettings();
+    // print(isNotification);
+  }
+
+  Future<void> _setButton() async {
+    final storage = Storage();
+
+    await storage.setSettings(SettingsKey.notification, true);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +28,14 @@ class Body extends StatelessWidget {
         child: Column(
           children: [
             SettingsList(),
+            TextButton(
+              onPressed: _pressButton,
+              child: Text('get'),
+            ),
+            TextButton(
+              onPressed: _setButton,
+              child: Text('set'),
+            ),
             const SizedBox(height: 20),
           ],
         ),
