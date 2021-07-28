77f8ce109999b7657bb261b4b18056986ae3151c
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-07-28 21:30:13 +0300

    Integration provider form

--- lib/screens/form/form_screen.dart
+++ lib/screens/form/form_screen.dart
@@ -3,45 +3,104 @@ import 'package:subscription_monitoring/models/Subscription.dart';
 import 'package:subscription_monitoring/models/Templates.dart';
 
 import 'components/body.dart';
+import 'form_model.dart';
+import 'form_provider.dart';
 
-class FormScreen extends StatelessWidget {
+class FormScreen extends StatefulWidget {
   static String routeName = '/form';
 
-  const FormScreen({Key? key}) : super(key: key);
+  FormScreen({Key? key}) : super(key: key);
+
+  @override
+  _FormScreenState createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  FormModel? _model;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    if (_model == null) {
+      final arguments =
+          ModalRoute.of(context)!.settings.arguments as FormArguments;
+
+      _model = FormModel(subscription: arguments.subscription);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    final arguments =
-        ModalRoute.of(context)!.settings.arguments as FormArguments;
+    // final arguments =
+    //     ModalRoute.of(context)!.settings.arguments as FormArguments;
 
-    final subscription = arguments.subscription;
-    final template = arguments.template;
-    final isUpdate = arguments.isUpdate;
+    // final subscription = arguments.subscription;
+    // final template = arguments.template;
+    // final isUpdate = arguments.isUpdate;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          subscription != null ? subscription.title : template!.title,
-          style: const TextStyle(color: Colors.black),
+    final model = _model;
+
+    if (model == null) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
         ),
+      );
+    }
+
+    return FormModelProvider(
+      model: model,
+      child: Scaffold(
+        appBar: _AppBar(),
+        // body: Body(),
       ),
-      body: Body(
-        subscription: subscription,
-        template: template,
-        isUpdate: isUpdate,
+    );
+  }
+}
+
+class ForModel {}
+
+class _AppBar extends StatelessWidget implements PreferredSizeWidget {
+  const _AppBar({Key? key}) : super(key: key);
+
+  @override
+  Size get preferredSize => const Size.fromHeight(60);
+
+  @override
+  Widget build(BuildContext context) {
+    final model = FormModelProvider.read(context)?.model;
+    final subscription = model?.subscription;
+
+    return AppBar(
+      title: Text(
+        subscription != null ? subscription.title : 'New subscription',
+        style: const TextStyle(color: Colors.black),
       ),
+      actions: [
+        TextButton(
+          style: ButtonStyle(
+            overlayColor:
+                MaterialStateColor.resolveWith((states) => Colors.transparent),
+          ),
+          onPressed: () => model?.save(),
+          child: const Text(
+            'Save',
+            style: TextStyle(color: Colors.black, fontSize: 18),
+          ),
+        ),
+        const SizedBox(width: 10)
+      ],
     );
   }
 }
 
 class FormArguments {
   final Subscription? subscription;
-  final Template? template;
   final bool isUpdate;
 
   FormArguments({
     this.subscription,
-    this.template,
     this.isUpdate = false,
   });
 }
