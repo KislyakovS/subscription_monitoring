e55e1001a27649f8e3256e0a3274fb0c935a2be8
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-07-28 20:56:06 +0300

    Added action appBar list subscription screen

--- lib/screens/list_subscription/list_subscription_screen.dart
+++ lib/screens/list_subscription/list_subscription_screen.dart
@@ -16,14 +16,35 @@ class ListSubscriptionScreen extends StatelessWidget {
     return ListSubscriptionModelProvider(
       model: _model,
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'New Subscription',
-            style: TextStyle(color: Colors.black),
-          ),
-        ),
+        appBar: const _AppBar(),
         body: Body(),
       ),
+    );
+  }
+}
+
+class _AppBar extends StatelessWidget implements PreferredSizeWidget {
+  const _AppBar({Key? key}) : super(key: key);
+
+  @override
+  Size get preferredSize => const Size.fromHeight(60);
+
+  @override
+  Widget build(BuildContext context) {
+    final model = ListSubscriptionModelProvider.read(context)?.model;
+
+    return AppBar(
+      title: const Text(
+        'New Subscription',
+        style: TextStyle(color: Colors.black),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () => model?.showDetails(context, null),
+          icon: const Icon(Icons.add, size: 32),
+        ),
+        const SizedBox(width: 10),
+      ],
     );
   }
 }
