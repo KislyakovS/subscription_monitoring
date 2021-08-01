b3b42d1204eba3888981e8c3a23dfd821cd73781
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-08-01 16:44:27 +0300

    Init hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:subscription_monitoring/theme/colors.dart';
 
 import 'app.dart';
 
-void main() {
+void main() async {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
     systemNavigationBarColor: defaultBackground,
     statusBarIconBrightness: Brightness.light,
     systemNavigationBarIconBrightness: Brightness.dark,
   ));
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
 
   runApp(App());
 }
